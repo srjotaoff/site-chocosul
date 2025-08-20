848fe54c9fbc48bc5b9846635cee80c3af810ced
SrJota <102335464+srjotaoff@users.noreply.github.com>
2025-08-20 13:07:17 -0300

    Add files via upload

---
+++ solicitar_suporte.js
@@ -0,0 +1,103 @@
+const cpf_usuario = '00000000000'
+const nome_usuario = 'JOAO SILVA SOUZA JUNIOR'
+const contato_usuario = '73999982509'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const inputFile = document.getElementById("main_painel_formulario_botoes_anexo_input");
+    const labelFile = document.getElementById("main_painel_formulario_botoes_anexo_label");
+
+    if (inputFile && labelFile) {
+        inputFile.addEventListener("change", function () {
+            if (this.files && this.files.length > 0) {
+                labelFile.textContent = this.files[0].name; // mostra o nome do arquivo
+            } else {
+                labelFile.textContent = "Anexar arquivo"; // volta ao texto padrão
+            }
+        });
+    }
+});
+
+async function salvar_solicitacao() {
+    const webhookurl = 'https://chocosul.bitrix24.com.br/rest/270/mwze5xa0wbsh91l1/'
+
+    const opcao_selecionada = document.querySelector('input[name="main_painel_formulario_opcoes_lista"]:checked');
+
+    if (!opcao_selecionada) {
+        document.getElementById('main_painel_formulario_alerta').style.display = 'flex';
+        document.getElementById('main_painel_formulario_alerta').style.backgroundColor = 'red';
+        document.getElementById('main_painel_formulario_alerta').style.color = 'var(--var_cor_branco)';
+        document.getElementById('main_painel_formulario_alerta').querySelector('p').textContent = 'Por favor, selecione uma opção válida.';
+        return; // Interrompe a execução da função
+    }
+
+    const nome_tarefa = opcao_selecionada.parentElement.textContent.trim();
+    const descricao = document.getElementById('main_painel_formulario_opcoes_texto').value
+
+    if (nome_tarefa && descricao) {
+        const id_responsavel = 270
+        const prazo_tarefa = new Date(new Date().setHours(new Date().getHours() + 2)).toISOString()
+        const observadores = [id_responsavel]
+
+        const parametros_api = {
+            fields: {
+                TITLE: nome_tarefa, //O título da tarefa.
+                DESCRIPTION: `
+Nome do solicitante: ${nome_usuario}
+Contato: ${contato_usuario}          https://wa.me/${contato_usuario}
+CPF: ${cpf_usuario}
+
+
+${descricao}`, //A descrição da tarefa.
+                RESPONSIBLE_ID: id_responsavel, //O ID do usuário responsável pela tarefa.
+                DEADLINE: prazo_tarefa, //A data de prazo para a tarefa (no formato YYYY-MM-DDTHH:MM:SSZ)
+                AUDITORS: observadores, //Um array com os IDs dos usuários que serão observadores da tarefa.
+            }
+        }
+
+        try {
+            const response = await fetch(`${webhookurl}tasks.task.add`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(parametros_api),
+            })
+
+            const data = await response.json()
+
+            if (data.result) {
+                document.getElementById('main_painel_formulario_alerta').style.display = 'flex';
+                document.getElementById('main_painel_formulario_alerta').style.backgroundColor = 'green';
+                document.getElementById('main_painel_formulario_alerta').style.color = 'var(--var_cor_branco)';
+                document.getElementById('main_painel_formulario_alerta').querySelector('p').textContent = 'Solicitação registrada. Logo alguem entrara em contato.';
+            } else {
+                document.getElementById('main_painel_formulario_alerta').style.display = 'flex';
+                document.getElementById('main_painel_formulario_alerta').style.backgroundColor = 'red';
+                document.getElementById('main_painel_formulario_alerta').style.color = 'var(--var_cor_branco)';
+                document.getElementById('main_painel_formulario_alerta').querySelector('p').textContent = 'Erro ao criar a tarefa.';
+            }
+        } catch (error) {
+            document.getElementById('main_painel_formulario_alerta').style.display = 'flex';
+            document.getElementById('main_painel_formulario_alerta').style.backgroundColor = 'red';
+            document.getElementById('main_painel_formulario_alerta').style.color = 'var(--var_cor_branco)';
+            document.getElementById('main_painel_formulario_alerta').querySelector('p').textContent = 'Erro na requisição: ',error;
+        }
+    } else {
+        document.getElementById('main_painel_formulario_alerta').style.display = 'flex';
+        document.getElementById('main_painel_formulario_alerta').style.backgroundColor = 'red';
+        document.getElementById('main_painel_formulario_alerta').style.color = 'var(--var_cor_branco)';
+        document.getElementById('main_painel_formulario_alerta').querySelector('p').textContent = 'Selecione uma opção válida e informe detalhes sobre a solicitação.';
+    }
+}
+
+function mostrar_opcoes_lista(setor) {
+    const opcoes_listas = document.querySelectorAll('.main_painel_formulario_opcoes_lista');
+    opcoes_listas.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    document.getElementById('main_painel_formulario_opcoes_lista_' + setor).style.display = 'grid';
+    document.getElementById('main_painel_formulario_opcoes_lista_' + setor).style.gap = '1svh';
+    document.getElementById('main_painel_formulario_opcoes_lista_' + setor).style.gridtemplatecolumns = repeat(1, auto);
+    document.getElementById('main_painel_formulario_opcoes_lista_' + setor).style.flexDirection = 'column';
+}
